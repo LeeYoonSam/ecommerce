b00be66e26fd1c4f1a8f5577b9fb1bc4be379afa
YS <yoonsam86@gmail.com>
2020-08-30 21:09:10 +0900

    [MOAA 클론코딩] - 1. 헤더 따라 만들기

---
+++ README.md
@@ -0,0 +1,8 @@
+# 한복 사이트 만들기
+
+## 클론 코딩 연습
+- HTML 클론 코딩
+
+### 참고
+- [MOAA](https://www.amekaji-moaa.com/)
+- [쇼핑 클론 코딩](http://blog.naver.com/PostView.nhn?blogId=93immm&logNo=221354720166&parentCategoryNo=&categoryNo=&viewDate=&isShowPopularPosts=false&from=postView)
---
+++ src/main/kotlin/com/ys/bup/hanbok/HanbokApplication.kt
@@ -0,0 +1,11 @@
+package com.ys.bup.hanbok
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HanbokApplication
+
+fun main(args: Array<String>) {
+	runApplication<HanbokApplication>(*args)
+}
